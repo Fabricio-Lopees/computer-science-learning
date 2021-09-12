10dd35c329b4519f025964075ff68331fba03deb
Fabricio Lopes <prog.fabriciolopes@gmail.com>
2021-09-12 10:51:03 +0000

    python-for-everybody chapter07 exercise03

---
+++ exercises/01.python-for-everybody/chapter7/exercise03.py
@@ -0,0 +1,25 @@
+file_name = input('Enter file name: ')
+
+if file_name == 'na na boo boo':
+	print("NA NA BOO BOO TO YOU - You have been punk'd!")
+	exit()
+else:
+	try:
+		file = open(file_name)
+	except:
+		print('File cannot be opened')
+		exit()
+
+count = 0
+numbers = 0
+average = 0
+for line in file:
+	if line.startswith('X-DSPAM-Confidence'):
+		colon_position = line.find(':')
+		numbers = numbers + float(line[colon_position+1:])
+		count = count + 1
+
+if count != 0:
+	average = numbers / count
+
+print(average)
