70fc4c9753f337fb47b04ae9eed5723a555e8037
Fabricio Lopes <prog.fabriciolopes@gmail.com>
2021-09-22 16:41:29 +0000

    modify python for everybody chapter09 exercise01

--- exercises/01.python-for-everybody/chapter09/exercise01.py
+++ exercises/01.python-for-everybody/chapter09/exercise01.py
@@ -1,17 +1,13 @@
 file = open('words.txt')
 
 word_list = []
-
 words = {}
 
 for line in file:
 	word_list = word_list + line.split()
 
-
-value = 0
 for word in word_list:
 	words[word] = value
-	value = value + 1
 
 
 print(words)
