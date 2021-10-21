965dca4fc46a351d1960b8d8016a09c1f51371e2
Fabricio Lopes <prog.fabriciolopes@gmail.com>
2021-10-21 21:29:59 +0000

    python-for-everybody chapter04 ex01 - remaked

---
+++ exercises/01.python-for-everybody/chapter04/ex01.py
@@ -0,0 +1,4 @@
+# Exercise 1: Run the program on your system and see what numbers you get. Run the program more than once and see what numbers you get.
+# random.randint(5, 10)
+import random;
+print(random.randint(5, 10));
